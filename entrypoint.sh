8f68b6b5ffb94077b4f58df6492a78a6392bba6f
Brian Berlin <bri@nberl.in>
2020-05-21 13:17:05 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,9 @@ URI="https://api.github.com"
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
+cat "$GITHUB_EVENT_PATH"
+number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
 echo "curl -sSL -H \"${AUTH_HEADER}\" -H \"${API_HEADER}\" \"${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}"
 
 body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}")
